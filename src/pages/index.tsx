0057cac24749a9bc209fdcda7fda3b36160a07d4
nukrobzero <nukrobzero@hotmail.com>
2023-03-23 15:06:26 +0700

    add style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,15 +5,32 @@ export default function Home() {
   if (session) {
     return (
       <>
-        Signed in as {session.user?.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
+        <div className="flex flex-col justify-center items-center  justify-items-center h-screen ">
+          Signed in as{" "}
+          <span className=" bg-orange-100">{session.user?.email}</span>
+          {session.user?.name}
+          <br />
+          <button
+            className="mt-4 rounded-tl-lg bg-green-400 rounded-br-lg px-5 py-2 hover:bg-green-600"
+            onClick={() => signOut()}
+          >
+            Sign out
+          </button>
+        </div>
       </>
     );
   }
   return (
     <>
-      Not signed in <br />
-      <button onClick={() => signIn()}>Sign in</button>
+      <div className="flex flex-col justify-center items-center  justify-items-center h-screen ">
+        Not signed in <br />
+        <button
+          className="mt-4 rounded-tl-lg bg-green-400 rounded-br-lg px-5 py-2 hover:bg-green-600"
+          onClick={() => signIn()}
+        >
+          Sign in
+        </button>
+      </div>
     </>
   );
 }
