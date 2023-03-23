03c0f370f69dd80978629317ed4321ff74ab6e4b
nukrobzero <nukrobzero@hotmail.com>
2023-03-23 16:22:39 +0700

    set api check authenticated

---
+++ src/pages/api/dashboard/service.ts
@@ -0,0 +1,30 @@
+import { getSession } from "next-auth/react";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "@/lib/prismadb";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const session = await getSession({ req });
+  // Check if user is authenticated
+  if (!session) {
+    return res.status(401).json({ message: "Unauthorized." });
+  }
+
+  if (req.method === "GET") {
+    try {
+      res.status(200).json({ name: "Nukrob" });
+    } catch (e) {
+      res.status(500).json({ message: "Something went wrong!" });
+    }
+  }
+
+  // HTTP method not supported!
+  else {
+    res.setHeader("Allow", ["GET"]);
+    res
+      .status(405)
+      .json({ message: `HTTP method ${req.method} is not supported.` });
+  }
+}
