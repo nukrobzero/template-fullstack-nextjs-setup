dc6dce61c4aa34f1a810766d3f1a827cfaca917a
nukrobzero <nukrobzero@hotmail.com>
2023-04-11 17:13:55 +0700

    edit: change same file

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,16 +1,31 @@
 import { GetStaticProps } from "next";
 import { prisma } from "@/lib/prismadb";
+import Layout from "@/components/frontend/layout/layout";
+import Headers from "@/components/headerAllpage";
 
 interface Props {
   blogs: any;
 }
+("use client");
 
 export default function Blogs({ blogs }: Props) {
   console.log(blogs);
   return (
-    <div>
-      <h1>ths Blogs page</h1>
-    </div>
+    <Layout>
+      <div>
+        <Headers title="Blogs Page" content="Blogs Page" />
+      </div>
+      <div className="mt-80px max-w-screen-xl mx-auto">
+        <div className="flex flex-col justify-center items-center">
+          {blogs.map((data: any) => (
+            <ul key={data.id}>
+              <h1>{data.title}</h1>
+              <li dangerouslySetInnerHTML={{ __html: data.content }}></li>
+            </ul>
+          ))}
+        </div>
+      </div>
+    </Layout>
   );
 }
 
