a4b5cd9b42250b80d746b40fd69604601278d063
nukrobzero <nukrobzero@hotmail.com>
2023-05-03 15:51:18 +0700

    edit: delette localhost

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: false,
   images: {
-    domains: ["localhost", "drive.google.com"],
+    domains: ["drive.google.com"],
     formats: ["image/webp"],
   },
 };
